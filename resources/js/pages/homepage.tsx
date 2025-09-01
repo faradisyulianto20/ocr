d3966a05e6ebff72a975df92a5f262ff0b44bb34
Faradis Yulianto <faradisy20@gmail.com>
2025-09-02 00:06:04 +0700

    Initial commit

---
+++ resources/js/pages/homepage.tsx
@@ -0,0 +1,8 @@
+export default function Homepage() {
+    return (
+        <div>
+            <h1>Welcome to the Homepage</h1>
+            <p>This is the homepage of our application.</p>
+        </div>
+    );
+}
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,9 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
+    Route::get('homepage', function () {
+        return Inertia::render('homepage');
+    })->name('homepage');
 });
 
 require __DIR__.'/settings.php';
